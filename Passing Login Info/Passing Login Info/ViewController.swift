9944f74bab275ac2e9cceaf9008906c627fe9675
Anton Huisamen <antonmcde@icloud.com>
2018-08-04 10:24:23 +0200

    All segues not working yet!?
    The 2 segues from password and login does not seque

--- Passing Login Info/Passing Login Info/ViewController.swift
+++ Passing Login Info/Passing Login Info/ViewController.swift
@@ -10,22 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var loginButton: UIButton!
+    
     @IBOutlet weak var userName: UITextField!
     
+    @IBOutlet weak var forgotUserNameButton: UIButton!
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-       segue.destination.navigationItem.title = userName.text
+        guard let sender = sender as? UIButton else {return}
         
+        if sender == forgotUserNameButton{
+            segue.destination.navigationItem.title = "Forgot Username"}
+            
+            else if sender == forgotPasswordButton{
+            segue.destination.navigationItem.title = "Forgot Password"}
+            
+            else{
+                segue.destination.navigationItem.title = userName.text}
+ 
+    }
+    
+    @IBAction func forgotUserNameButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func forgotPasswordButtonPressed(_ sender: UIButton) {
     }
 }
 
