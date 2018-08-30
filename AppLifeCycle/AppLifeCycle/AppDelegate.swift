63bb10f50aca2f3a99fdd6dd2240761c15b60c69
Anton Huisamen <antonmcde@icloud.com>
2018-08-30 12:25:53 +0200

    fixed

--- AppLifeCycle/AppLifeCycle/AppDelegate.swift
+++ AppLifeCycle/AppLifeCycle/AppDelegate.swift
@@ -13,36 +13,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     var viewController: UIViewController?
-    var vc = ViewControllerOne()
-    
+    var finishLaunchingCount = 0
+    var becomeActiveCount = 0
+    var resighnActiveCount = 0
+    var enterBackgroundCount = 0
+    var enterForegroundCount = 0
+    var willTerminateCount = 0
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        viewController = window?.rootViewController as? ViewControllerOne
         print("Did finish launching")
+        finishLaunchingCount += 1
         return true
     }
     
     func applicationWillResignActive(_ application: UIApplication) {
+        becomeActiveCount += 1
         print("Will resighn active")
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
         print("Application did enter backgrount")
+        resighnActiveCount += 1
+        
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
         print("Will enter foreground")
+        enterBackgroundCount += 1
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
     
     func applicationDidBecomeActive(_ application: UIApplication) {
         print("Did become active")
+        enterForegroundCount += 1
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
         print("will terminate")
+        willTerminateCount += 1
+        
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
--- AppLifeCycle/AppLifeCycle/ViewControllerOne.swift
+++ AppLifeCycle/AppLifeCycle/ViewControllerOne.swift
@@ -11,31 +11,25 @@ import UIKit
 class ViewControllerOne: UIViewController {
     
     @IBOutlet weak var didFinishLaunching: UILabel!
-    var finishLaunchingCount = 0 
     @IBOutlet weak var didBecomeActive: UILabel!
-    var becomeActiveCount = 0
     @IBOutlet weak var willResighnActive: UILabel!
-    var resighnActiveCount = 0
     @IBOutlet weak var didEnterBackground: UILabel!
-    var enterBackgroundCount = 0
     @IBOutlet weak var willEnterForeground: UILabel!
-    var enterForegroundCount = 0
     @IBOutlet weak var wilTerminate: UILabel!
-    var willTerminateCount = 0
     
-    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
 
     override func viewDidLoad() {
         super.viewDidLoad()
         updateView()
     }
     func updateView(){
-        didFinishLaunching.text = "The App did Fisnish Launching \(finishLaunchingCount) times"
-        didBecomeActive.text = "The App did become active \(becomeActiveCount) times"
-        willResighnActive.text = "The App will resign active \(resighnActiveCount) times"
-        didEnterBackground.text = "The App did enter background \(enterBackgroundCount) times"
-        willEnterForeground.text = "The App will enter foreground \(enterForegroundCount) times"
-        wilTerminate.text = "The App will terminate \(willTerminateCount) times"
+        didFinishLaunching.text = "The App did Fisnish Launching \(appDelegate.finishLaunchingCount) times"
+        didBecomeActive.text = "The App did become active \(appDelegate.becomeActiveCount) times"
+        willResighnActive.text = "The App will resign active \(appDelegate.resighnActiveCount) times"
+        didEnterBackground.text = "The App did enter background \(appDelegate.enterBackgroundCount) times"
+        willEnterForeground.text = "The App will enter foreground \(appDelegate.enterForegroundCount) times"
+        wilTerminate.text = "The App will terminate \(appDelegate.willTerminateCount) times"
 
     }
 
