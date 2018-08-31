3b1eb4304befc457c9ae6150db51ce17a20dc67e
Anton Huisamen <antonmcde@icloud.com>
2018-08-31 17:58:38 +0200

    web extension

--- Working with the Web.playground/Contents.swift
+++ Working with the Web.playground/Contents.swift
@@ -7,12 +7,24 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 
 
 
-let url = URL(string: "https://www.apple.com")!
+let url = URL(string: "GET https://api.nasa.gov/planetary/apodDEMO_KEY")!
 let task = URLSession.shared.dataTask(with: url) {(data, response, error)
     in
-    if let data = data {print(data)}
+    if let data = data,
+        let string = String(data: data, encoding: .utf8){
+        print(string)
+        
+    }
+    
 }
 
 task.resume()
 
 
+extension URL {
+    func withQueries(_ quereis :[String: String]) -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = quereis.flatMap { URLQueryItem(name: $0.0, value: $0.1)}
+        return components?.url
+    }
+}
